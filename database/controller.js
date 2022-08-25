c00c8091bdddb0a03c7e9b62c8b9b9c69d568759
leobh76 <lbhurovich@gmail.com>
2022-08-25 13:12:37 +0300

    added returns

--- database/controller.js
+++ database/controller.js
@@ -20,9 +20,9 @@ export async function getUser(req, res) {
     console.log("controller")
     if (userId) {
       const user = await Users.findById(userId);
-      res.status(200).json(user);
+      return res.status(200).json(user);
     }
-    res.status(404).json({ error: "User not selected!" });
+    return res.status(404).json({ error: "User not selected!" });
   } catch (error) {
     res.status(404).json({ error: "Cannot get the user!" });
   }
