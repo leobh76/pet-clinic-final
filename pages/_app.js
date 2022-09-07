62141e9dcc188ba3f2749a693feae5fe2c915d0e
leobh76 <lbhurovich@gmail.com>
2022-09-07 16:52:11 +0300

    added Chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,20 @@ import "../styles/globals.css";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
 
 // creating a client
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-    </QueryClientProvider>
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </QueryClientProvider>
+    </ChakraProvider>
   );
 }
 
