68f24be62b9c21350b91414feb3cd1a6d1b0ff2f
Leonardo B. HUrovich <105162659+leobh76@users.noreply.github.com>
2022-09-11 20:11:48 +0300

    search by ownerName and petName enabled

--- components/table.js
+++ components/table.js
@@ -17,9 +17,40 @@ import {
   PopoverBody,
   PopoverArrow,
 } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 
 export default function Table() {
-  const { isLoading, isError, data, error } = useQuery("users", getUsers);
+  const [appointments, setAppointments] = useState([]);
+  const [display, setDisplay] = useState([]);
+  const [filter1, setFilter1] = useState("");
+  const [filter2, setFilter2] = useState("");
+
+  const { isLoading, isError, data, error } = useQuery("users", getUsers, {
+    onSuccess: (data) => {
+      setAppointments(data);
+      setDisplay(appointments);
+    },
+  });
+
+  useEffect(() => {
+    debugger;
+    if (!filter1 && !filter2) {
+      setDisplay(appointments);
+    } else {
+      let filteredAppointments;
+      if (filter1) {
+        filteredAppointments = appointments.filter((item) =>
+          item.ownerName.toLowerCase().includes(filter1.toLowerCase())
+        );
+      }
+      if (filter2) {
+        filteredAppointments = filteredAppointments.filter((item) =>
+          item.petName.toLowerCase().includes(filter2.toLowerCase())
+        );
+      }
+      setDisplay(filteredAppointments);
+    }
+  }, [filter1, filter2]);
 
   if (isLoading) return <div>Appointments are loading...</div>;
   if (isError) return <div>Error: {error}</div>;
@@ -36,6 +67,7 @@ export default function Table() {
               bg="white"
               width="50"
               placeholder="Search for an owner..."
+              onChange={(e) => setFilter1(e.target.value)}
             />
           </th>
           <th className="align-top px-5 py-2">
@@ -49,6 +81,7 @@ export default function Table() {
               bg="white"
               width="50"
               placeholder="Search for a pet..."
+              onChange={(e) => setFilter2(e.target.value)}
             />
           </th>
           <th className="align-top px-5 py-2">
@@ -80,7 +113,7 @@ export default function Table() {
         </tr>
       </thead>
       <tbody className="bg-gray-200">
-        {data.map((obj, i) => (
+        {display.map((obj, i) => (
           <Tr {...obj} key={i} />
         ))}
       </tbody>
